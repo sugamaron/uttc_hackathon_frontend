13d53ca84645a29d6ee295f733811438a71c2b71
sugamaron <sugamaron10@gmail.com>
2023-10-23 21:32:49 +0900

    認証画面変更

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { LoginForm } from './LoginForm';
 import { onAuthStateChanged } from "firebase/auth";
@@ -19,20 +18,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <LoginForm />
       <SignUpForm />
       {/* ログインしていないと見られないコンテンツは、loginUserがnullの場合表示しない */}
